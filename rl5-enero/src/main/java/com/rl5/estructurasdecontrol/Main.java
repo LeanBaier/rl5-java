65eeef51e265b6ae06f718bad108ca02b9a99ad6
Leandro Baier <leanbaierpersonal@gmail.com>
2024-01-10 18:56:13 -0300

    Se agregan estructuras de control y bucles

--- rl5-enero/src/main/java/com/rl5/estructurasdecontrol/Main.java
+++ rl5-enero/src/main/java/com/rl5/estructurasdecontrol/Main.java
@@ -5,6 +5,78 @@ public class Main {
     public static void main(String[] args) {
         //Estructuras de control - IF / Switch
 
+        /*Operadores
+            ! -> Negación
+            boolean b = !false; //true
+            ++ -> Incremento
+            int a = 1;
+            a++; //a vale 2
+            ++a; //a vale 3
+            -- -> Decremento
+            a--; //a vale 2
+            --a; //a vale 1
+
+            + -> Suma
+            int resultado = 2 + 5; //resutado vale 7
+            - -> Resta
+            int resultado = 2 - 5; //resultado vale -3
+            * -> Multiplicación
+            int resultado = 2 * 5; //resultado vale 10
+            / -> Divición
+            int resultado = 10 / 5; //resultado vale 2
+            % -> Resto
+            int resultado = 11 / 5; //resultado vale 1
+
+            true -> Verdadero
+            false -> Falso
+
+            = -> Asignar valor
+            += -> Acumular
+            int acumulador = 1;
+            acumulador += 3; //acumulador vale 4
+            -= -> Decrementar
+            acumulador -= 1; //acumulador vale 3
+
+            Comparadores
+            == -> Igual a
+            1 == 1 // true
+            1 == 2 // false
+
+            != -> Distinto a
+            1 != 1 //false
+            1 != 2 //true
+
+            < -> Menor
+            1 < 1 //false
+            1 < 2 //true
+
+            <= -> Menor o igual
+            1 <= 1 //true
+            1 <= 0 //false
+            1 <= 2 //true
+
+            > -> Mayor
+            1 > 1 //false
+            2 > 1 //true
+
+            >= -> Mayor o igual
+            1 >= 1 //true
+            1 >= 2 //false
+            1 >= 0 //true
+
+            && -> Y / And
+            true && true -> true
+            true && false -> false
+            false && false -> false
+            false && true -> false
+
+            || -> O / Or
+            true || true -> true
+            true || false -> true
+            false || false -> false
+            false || true -> true
+        */
+
     }
 
 }
